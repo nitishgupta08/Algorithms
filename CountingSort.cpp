2f8e327591132fec090c2893b9b1249841365cae
nitishgupta <nitishkumargupta.1729@gmail.com>
2021-08-15 12:34:01 +0530

    Added Counting Sort

--- CountingSort.cpp
+++ CountingSort.cpp
@@ -1,29 +1,43 @@
+/*
+ * Created Date: Friday, February 26th 2021, 11:27:25 pm
+ * Author: Nitish Kumar Gupta
+ * 
+ * Copyright (c) 2021 nitishgupta
+ */
+
+
 #include <iostream>
 
 using namespace std;
 
 void CountingSort(int *arr,int n) {
+    //Finding Maximum in arr
     int max = arr[0];
     for(int i=0;i<n;i++)
         if(arr[i] > max)
             max = arr[i];
     
+    //Creating a count array of size max+1;
     int *count;
     count = new int[max+1];
-    int B[n];
+    for(int i=0;i<max+1;i++) count[i] = 0;
 
+    //Filling the array;
     for(int i=0;i<n;i++) 
         count[arr[i]]++;
-    
-    for(int i=1;i<max+1;i++)
-        count[i] = count[i] + count[i-1];
-    
-    for(int i=n-1;i>=0;i--) {
-        B[count[arr[i]]-1] = arr[i];
-        count[arr[i]]--;
+
+    //Filling sorted elements back to the original array
+    int j=0;
+    for(int i=0;i<max+1;i++) {
+        if(count[i] != 0) {
+            while(count[i] > 0) {
+                arr[j++] = i;
+                count[i]--;
+            }
+        } 
     }
-    for(int i=0;i<n;i++)
-        arr[i] = B[i];
+
+    //release space
     delete[] count;
 }
 
